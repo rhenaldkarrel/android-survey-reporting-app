5c1b55f72979e0fd2787e3ec0f038599a11d46b3
Rhenald <renaldkarel@gmail.com>
2023-04-10 16:07:20 +0700

    feat: integrate current location on map

--- screens/Map.js
+++ screens/Map.js
@@ -1,29 +1,72 @@
 import React from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { StatusBar, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { regionFrom } from '../lib/helpers/regionFrom';
 import { Box, Button, Heading, Text, VStack } from 'native-base';
 import { createOpenLink } from 'react-native-open-maps';
+import * as Location from 'expo-location';
 
 export default function Map({ navigation }) {
-	const coordinates = regionFrom(-6.129759815614638, 106.9556811104887, 2);
+	const [myLocation, setMyLocation] = React.useState({
+		latitude: 0,
+		longitude: 0,
+		latitudeDelta: 2,
+		longitudeDelta: 2,
+	});
+	const [message, setMessage] = React.useState('');
+	const map = React.useRef();
 	const coordinates2 = regionFrom(-6.11934474821684, 106.956970205967, 2);
 
+	const isLocationEmpty =
+		myLocation.latitude === 0 || myLocation.longitude === 0;
+
+	React.useEffect(() => {
+		(async () => {
+			setMessage('Mengambil lokasi...');
+			try {
+				let { status } = await Location.requestForegroundPermissionsAsync();
+
+				if (status !== 'granted') {
+					throw new Error('Akses tidak diberikan.');
+				}
+
+				let location = await Location.getCurrentPositionAsync({});
+				const {
+					coords: { latitude, longitude },
+				} = location;
+
+				const myLocation = regionFrom(latitude, longitude, 2);
+				const allCoords = [myLocation, coordinates2];
+
+				setMyLocation(myLocation);
+				map.current.fitToCoordinates(allCoords, {
+					edgePadding: { top: 40, right: 40, bottom: 40, left: 40 },
+					animated: true,
+				});
+			} catch (err) {
+				setMessage(err.message);
+			} finally {
+				setMessage('');
+			}
+		})();
+	}, []);
+
 	const openGoogleMapsNavigation = createOpenLink({
-		start: `${coordinates.latitude}, ${coordinates.longitude}`,
+		start: `${myLocation.latitude}, ${myLocation.longitude}`,
 		end: 'SMPN 244 Jakarta',
 		provider: 'google',
 	});
 
 	return (
 		<View style={styles.container}>
-			<MapView style={styles.map} initialRegion={coordinates}>
-				<Marker
-					coordinate={coordinates}
-					title='title'
-					description='description'
-				/>
+			<MapView
+				style={styles.map}
+				showsUserLocation={true}
+				initialRegion={myLocation}
+				ref={map}
+				followsUserLocation={true}
+			>
 				<Marker
 					coordinate={coordinates2}
 					title='title'
@@ -48,7 +91,9 @@ export default function Map({ navigation }) {
 							Lokasi Anda Sekarang
 						</Heading>
 						<Text Text fontWeight='bold' fontSize={18}>
-							8.123124 meter
+							{!isLocationEmpty
+								? `${myLocation.latitude}, ${myLocation.longitude}`
+								: message}
 						</Text>
 					</Box>
 					<Box>
@@ -81,6 +126,7 @@ export default function Map({ navigation }) {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		marginTop: StatusBar.currentHeight,
 	},
 	map: {
 		position: 'absolute',
