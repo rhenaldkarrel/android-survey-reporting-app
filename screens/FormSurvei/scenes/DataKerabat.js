44914e57dbc3cbd234c1fe8fbc82f3a0990fc46c
Rhenald <renaldkarel@gmail.com>
2023-04-19 14:03:19 +0700

    feat: data kerabat scene

---
+++ screens/FormSurvei/scenes/DataKerabat.js
@@ -0,0 +1,236 @@
+import {
+	Button,
+	FormControl,
+	HStack,
+	Input,
+	ScrollView,
+	Select,
+	Text,
+	VStack,
+} from 'native-base';
+import { Controller, useForm } from 'react-hook-form';
+
+export default function DataKerabat({ debiturId }) {
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+		watch,
+	} = useForm({
+		defaultValues: {
+			email: '',
+			name: '',
+			password: '',
+			confirmPassword: '',
+		},
+	});
+
+	return (
+		<ScrollView
+			contentContainerStyle={{ paddingVertical: 32, paddingHorizontal: 16 }}
+		>
+			<VStack space='12px'>
+				<Text fontStyle='italic' fontSize='12px' color='gray.500'>
+					Isi data referensi (kerabat tidak serumah) bila ada.
+				</Text>
+				<FormControl>
+					<FormControl.Label>Nama Lengkap (sesuai KTP)</FormControl.Label>
+					<Controller
+						control={control}
+						render={({ field: { onChange, onBlur, value } }) => (
+							<Input
+								onBlur={onBlur}
+								onChangeText={(val) => onChange(val)}
+								value={value}
+							/>
+						)}
+						name='nama_lengkap'
+						defaultValue=''
+						shouldUnregister={true}
+					/>
+				</FormControl>
+				<FormControl>
+					<FormControl.Label>Jenis Kelamin</FormControl.Label>
+					<Controller
+						control={control}
+						render={({ field: { onChange, onBlur, value } }) => (
+							<Select
+								placeholder='Jenis Kelamin'
+								selectedValue={value}
+								onBlur={onBlur}
+								onValueChange={(val) => onChange(val)}
+							>
+								<Select.Item label='Laki-laki' value='laki-laki' />
+								<Select.Item label='Perempuan' value='perempuan' />
+							</Select>
+						)}
+						name='status_pernikahan'
+						defaultValue=''
+						shouldUnregister={true}
+					/>
+				</FormControl>
+				<FormControl>
+					<FormControl.Label>Alamat (sesuai KTP)</FormControl.Label>
+					<VStack space='12px'>
+						<Controller
+							control={control}
+							render={({ field: { onChange, onBlur, value } }) => (
+								<Input
+									onBlur={onBlur}
+									onChangeText={(val) => onChange(val)}
+									value={value}
+									placeholder='Alamat'
+								/>
+							)}
+							name='alamat'
+							defaultValue=''
+							shouldUnregister={true}
+						/>
+						<HStack space='12px'>
+							<Controller
+								control={control}
+								render={({ field: { onChange, onBlur, value } }) => (
+									<Input
+										onBlur={onBlur}
+										onChangeText={(val) => onChange(val)}
+										value={value}
+										placeholder='RT'
+										flex={1}
+									/>
+								)}
+								name='rt'
+								defaultValue=''
+								shouldUnregister={true}
+							/>
+							<Controller
+								control={control}
+								render={({ field: { onChange, onBlur, value } }) => (
+									<Input
+										onBlur={onBlur}
+										onChangeText={(val) => onChange(val)}
+										value={value}
+										placeholder='RW'
+										flex={1}
+									/>
+								)}
+								name='rw'
+								defaultValue=''
+								shouldUnregister={true}
+							/>
+							<Controller
+								control={control}
+								render={({ field: { onChange, onBlur, value } }) => (
+									<Input
+										onBlur={onBlur}
+										onChangeText={(val) => onChange(val)}
+										value={value}
+										placeholder='Kode Pos'
+										flex={1}
+									/>
+								)}
+								name='kode_pos'
+								defaultValue=''
+								shouldUnregister={true}
+							/>
+						</HStack>
+						<HStack space='12px'>
+							<Controller
+								control={control}
+								render={({ field: { onChange, onBlur, value } }) => (
+									<Input
+										onBlur={onBlur}
+										onChangeText={(val) => onChange(val)}
+										value={value}
+										placeholder='Kelurahan'
+										flex={1}
+									/>
+								)}
+								name='kelurahan'
+								defaultValue=''
+								shouldUnregister={true}
+							/>
+							<Controller
+								control={control}
+								render={({ field: { onChange, onBlur, value } }) => (
+									<Input
+										onBlur={onBlur}
+										onChangeText={(val) => onChange(val)}
+										value={value}
+										placeholder='Kecamatan'
+										flex={1}
+									/>
+								)}
+								name='kecamatan'
+								defaultValue=''
+								shouldUnregister={true}
+							/>
+						</HStack>
+						<Controller
+							control={control}
+							render={({ field: { onChange, onBlur, value } }) => (
+								<Input
+									onBlur={onBlur}
+									onChangeText={(val) => onChange(val)}
+									value={value}
+									placeholder='Kota'
+								/>
+							)}
+							name='kota'
+							defaultValue=''
+							shouldUnregister={true}
+						/>
+					</VStack>
+				</FormControl>
+				<FormControl>
+					<FormControl.Label>No. HP</FormControl.Label>
+					<Controller
+						control={control}
+						render={({ field: { onChange, onBlur, value } }) => (
+							<Input
+								onBlur={onBlur}
+								onChangeText={(val) => onChange(val)}
+								value={value}
+							/>
+						)}
+						name='no_hp'
+						defaultValue=''
+						shouldUnregister={true}
+					/>
+				</FormControl>
+				<FormControl>
+					<FormControl.Label>No. Telepon</FormControl.Label>
+					<Controller
+						control={control}
+						render={({ field: { onChange, onBlur, value } }) => (
+							<Input
+								onBlur={onBlur}
+								onChangeText={(val) => onChange(val)}
+								value={value}
+							/>
+						)}
+						name='no_telp'
+						defaultValue=''
+						shouldUnregister={true}
+					/>
+				</FormControl>
+				<FormControl>
+					<FormControl.Label>Hubungan Dengan Pemohon</FormControl.Label>
+					<Controller
+						control={control}
+						render={({ field: { onChange, onBlur, value } }) => (
+							<Input
+								onBlur={onBlur}
+								onChangeText={(val) => onChange(val)}
+								value={value}
+							/>
+						)}
+						name='hubungan_dengan_pemohon'
+						defaultValue=''
+						shouldUnregister={true}
+					/>
+				</FormControl>
+				<Button bgColor='primary.400'>Simpan Data</Button>
+			</VStack>
+		</ScrollView>
+	);
+}
