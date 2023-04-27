c2c5758348c0c13536514df965ca30b9ba2b153c
Rhenald <renaldkarel@gmail.com>
2023-04-27 12:05:43 +0700

    feat: integrate data asuransi get api

--- screens/FormPermohonan/scenes/DataAsuransi.js
+++ screens/FormPermohonan/scenes/DataAsuransi.js
@@ -9,19 +9,24 @@ import {
 	VStack,
 } from 'native-base';
 import { Controller, useForm } from 'react-hook-form';
+import { useDataAsuransi } from '../../../api/form-permohonan';
 
-export default function DataAsuransi({ debiturId }) {
+export default function DataAsuransi({ debiturId, formPermohonanId }) {
+	const { dataAsuransi, postDataAsuransi } = useDataAsuransi(formPermohonanId);
+
 	const {
 		control,
 		handleSubmit,
 		formState: { errors },
 		watch,
 	} = useForm({
-		defaultValues: {
-			email: '',
-			name: '',
-			password: '',
-			confirmPassword: '',
+		values: {
+			jenis_asuransi: dataAsuransi.jenis_asuransi,
+			nilai_pertanggungan: dataAsuransi.nilai_pertanggungan?.toString(),
+			premi_dibayar_secara: dataAsuransi.premi_dibayar_secara,
+			nominal_premi_dibayar: dataAsuransi.nominal_premi_dibayar?.toString(),
+			periode_premi_dibayar: dataAsuransi.periode_premi_dibayar?.toString(),
+			jaminan_tambahan: dataAsuransi.jaminan_tambahan,
 		},
 	});
 
@@ -41,8 +46,11 @@ export default function DataAsuransi({ debiturId }) {
 								onBlur={onBlur}
 								onValueChange={(val) => onChange(val)}
 							>
-								<Select.Item label='Total Loss Only (TLO)' value='tlo' />
-								<Select.Item label='All Risk' value='all_risk' />
+								<Select.Item
+									label='Total Loss Only (TLO)'
+									value='Total Loss Only (TLO)'
+								/>
+								<Select.Item label='All Risk' value='All Risk' />
 							</Select>
 						)}
 						name='jenis_asuransi'
@@ -59,8 +67,8 @@ export default function DataAsuransi({ debiturId }) {
 								onBlur={onBlur}
 								onChangeText={(val) => onChange(val)}
 								value={value}
-                keyboardType='number-pad'
-                InputLeftElement={<Text ml="8px">Rp</Text>}
+								keyboardType='number-pad'
+								InputLeftElement={<Text ml='8px'>Rp</Text>}
 							/>
 						)}
 						name='nilai_pertanggungan'
@@ -79,8 +87,8 @@ export default function DataAsuransi({ debiturId }) {
 								onBlur={onBlur}
 								onValueChange={(val) => onChange(val)}
 							>
-								<Select.Item label='Tunai' value='tunai' />
-								<Select.Item label='Angsuran' value='angsuran' />
+								<Select.Item label='Tunai' value='Tunai' />
+								<Select.Item label='Angsuran' value='Angsuran' />
 							</Select>
 						)}
 						name='premi_dibayar_secara'
@@ -88,7 +96,7 @@ export default function DataAsuransi({ debiturId }) {
 						shouldUnregister={true}
 					/>
 				</FormControl>
-        <FormControl>
+				<FormControl>
 					<FormControl.Label>Nominal Premi Dibayar</FormControl.Label>
 					<Controller
 						control={control}
@@ -97,8 +105,8 @@ export default function DataAsuransi({ debiturId }) {
 								onBlur={onBlur}
 								onChangeText={(val) => onChange(val)}
 								value={value}
-                keyboardType='number-pad'
-                InputLeftElement={<Text ml="8px">Rp</Text>}
+								keyboardType='number-pad'
+								InputLeftElement={<Text ml='8px'>Rp</Text>}
 							/>
 						)}
 						name='nominal_premi_dibayar'
@@ -106,7 +114,7 @@ export default function DataAsuransi({ debiturId }) {
 						shouldUnregister={true}
 					/>
 				</FormControl>
-        <FormControl>
+				<FormControl>
 					<FormControl.Label>Periode Premi Dibayar</FormControl.Label>
 					<Controller
 						control={control}
@@ -115,16 +123,16 @@ export default function DataAsuransi({ debiturId }) {
 								onBlur={onBlur}
 								onChangeText={(val) => onChange(val)}
 								value={value}
-                keyboardType='number-pad'
-                InputRightElement={<Text mr="8px">bulan</Text>}
+								keyboardType='number-pad'
+								InputRightElement={<Text mr='8px'>bulan</Text>}
 							/>
 						)}
-						name='nominal_premi_dibayar'
+						name='periode_premi_dibayar'
 						defaultValue=''
 						shouldUnregister={true}
 					/>
 				</FormControl>
-        <FormControl>
+				<FormControl>
 					<FormControl.Label>Jaminan Tambahan</FormControl.Label>
 					<Controller
 						control={control}
