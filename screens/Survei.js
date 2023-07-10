c81d38a0308c8942b339ae0426231ebeed970729
Rhenald <renaldkarel@gmail.com>
2023-07-10 19:50:24 +0700

    fix: scrollable flatlist survei

--- screens/Survei.js
+++ screens/Survei.js
@@ -15,25 +15,24 @@ export default function Survei() {
 	}, []);
 
 	return (
-		<View marginTop={StatusBar.currentHeight} refres>
-			<VStack space='8px' m='16px'>
-				<Heading fontWeight='medium' fontSize='16px'>
-					Daftar Survei
-				</Heading>
-				{dataPengajuan.length > 0 ? (
-					<FlatList
-						data={dataPengajuan}
-						keyExtractor={({ _id }) => _id}
-						ItemSeparatorComponent={<View style={{ height: 16 }} />}
-						renderItem={({ item }) => <DebiturCard onRefresh={onRefresh} formPengajuanData={item} />}
-						refreshControl={
-							<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-						}
-					/>
-				) : (
-					<Text>No data found</Text>
-				)}
-			</VStack>
-		</View>
+		<FlatList
+      style={{ marginTop: StatusBar.currentHeight }}
+      contentContainerStyle={{ padding: 16 }}
+      data={dataPengajuan}
+      keyExtractor={({ _id }) => _id}
+      ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+      renderItem={({ item }) => (
+        <DebiturCard onRefresh={onRefresh} formPengajuanData={item} />
+      )}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+      ListHeaderComponent={
+        <Heading fontWeight='medium' fontSize='16px'>
+          Daftar Survei
+        </Heading>
+      }
+      ListEmptyComponent={<Text>No data found</Text>}
+    />
 	);
 }
